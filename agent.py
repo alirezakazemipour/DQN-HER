628424f7a738bd4273e2e305e834da3ea970947e
alirezakazemipour <kazemipour.alireza@gmail.com>
2020-03-27 21:33:23 +0430

    Add learn function

--- agent.py
+++ agent.py
@@ -2,20 +2,22 @@ from model import DQN
 from torch.optim.adam import Adam
 from torch.nn import MSELoss
 import numpy as np
-from replay_memory import Memory
+from replay_memory import Memory, Transition
 import torch
 
 
 class Agent:
-    def __init__(self, n_bits, lr, memory_size):
+    def __init__(self, n_bits, lr, memory_size, batch_size, gamma):
         self.n_bits = n_bits
         self.lr = lr
+        self.gamma = gamma
+        self.batch_size = batch_size
         self.memory_size = memory_size
         self.memory = Memory(self.memory_size)
 
         self.device = "cpu"
-        self.model = DQN(n_inputs=self.n_bits).to(self.device)
-        self.target_model = DQN(n_inputs=self.n_bits).to(self.device)
+        self.model = DQN(n_inputs=2 * self.n_bits).to(self.device)
+        self.target_model = DQN(n_inputs=2 * self.n_bits).to(self.device)
         self.target_model.load_state_dict(self.model.state_dict())
         self.target_model.eval()
         self.opt = Adam(self.model.parameters(), lr=self.lr)
@@ -30,7 +32,7 @@ class Agent:
         else:
             states = torch.Tensor(states, device=self.device)
             goals = torch.Tensor(goals, device=self.device)
-            action = self.model(states, goals).max(dim=-1).item()
+            action = self.model(states, goals).max(dim=-1)[1].detach().numpy()
             action = np.clip(action, 0, self.n_bits)
 
         return action
@@ -41,8 +43,46 @@ class Agent:
     def store(self, state, action, reward, done, next_state, goal):
         state = torch.Tensor(state, device=self.device)
         reward = torch.Tensor([reward], device=self.device)
-        action = torch.Tensor(action, device=self.device)
+        action = torch.Tensor([action], device=self.device)
         next_state = torch.Tensor(next_state, device=self.device)
         done = torch.Tensor([done], device=self.device)
         goal = torch.Tensor(goal, device=self.device)
         self.memory.push(state, action, reward, done, next_state, goal)
+
+    def unpack_batch(self, batch):
+
+        batch = Transition(*zip(*batch))
+
+        states = torch.cat(batch.state).to(self.device).view(self.batch_size, self.n_bits)
+        actions = torch.cat(batch.action).to(self.device)
+        rewards = torch.cat(batch.reward).to(self.device)
+        next_states = torch.cat(batch.next_state).to(self.device).view(self.batch_size, self.n_bits)
+        dones = torch.cat(batch.done).to(self.device)
+        goals = torch.cat(batch.goal).to(self.device).view(self.batch_size, self.n_bits)
+
+        return states, actions, rewards, dones, next_states, goals
+
+    def learn(self):
+        if len(self.memory) < self.batch_size:
+            return 0
+        batch = self.memory.sample(self.batch_size)
+        states, actions, rewards, dones, next_states, goals = self.unpack_batch(batch)
+
+        with torch.no_grad():
+            target_q = rewards + self.gamma * self.target_model(next_states, goals).max(-1)[0] * (1 - dones)
+
+        q = self.model(states, goals)[actions.long()]
+        loss = self.loss_fn(q, target_q.view(self.batch_size, 1))
+
+        self.opt.zero_grad()
+        loss.backward()
+        self.opt.step()
+
+        self.soft_update_of_target_network(self.model, self.target_model)
+
+        return loss.item()
+
+    @staticmethod
+    def soft_update_of_target_network(local_model, target_model, tau=0.05):
+        for target_param, local_param in zip(target_model.parameters(), local_model.parameters()):
+            target_param.data.copy_(tau * local_param.data + (1.0 - tau) * target_param.data)
--- main.py
+++ main.py
@@ -5,15 +5,16 @@ import numpy as np
 
 n_bits = 11
 lr = 1e-3
-MAX_EPISODE_NUM = 1000
-memory_size = 1000
-
+gamma = 0.98
+MAX_EPISODE_NUM = 10000
+memory_size = 10000
+batch_size = 128
 k_future = 4
 
 if __name__ == "__main__":
     print(f"Number of bits:{n_bits}")
     env = Env(n_bits)
-    agent = Agent(n_bits=n_bits, lr=lr, memory_size=memory_size)
+    agent = Agent(n_bits=n_bits, lr=lr, memory_size=memory_size, batch_size=batch_size, gamma=gamma)
 
     for episode_num in range(MAX_EPISODE_NUM):
         state, goal = env.reset()
@@ -24,7 +25,8 @@ if __name__ == "__main__":
             action = agent.choose_action(state, goal)
             next_state, reward, done, _ = env.step(action)
             agent.store(state, action, reward, done, next_state, goal)
-            episode.append((state, action, reward, done, next_state))
+            episode.append((state.copy(), action, reward, done, next_state.copy()))
+            loss = agent.learn()
             state = next_state
             episode_reward += reward
 
@@ -32,9 +34,17 @@ if __name__ == "__main__":
         for i, transition in enumerate(episode):
             state, action, reward, done, next_state = transition
             future_transitions = random.choices(episode[i:], k=k_future)
-            for goal in future_transitions:
-                new_goal = future_transitions[-2]
+            for future_transition in future_transitions:
+                new_goal = future_transition[-1]
                 if np.sum(next_state == new_goal) == n_bits:
                     reward = 0
                 else:
                     reward = -1
+
+                agent.store(state, action, reward, done, next_state, new_goal)
+
+        agent.update_epsilon()
+        if episode_num % 50 == 0:
+            print(f"Ep:{episode_num}| "
+                  f"Ep_r:{episode_reward:3.3f}| "
+                  f"Loss:{loss:3.3f}")
